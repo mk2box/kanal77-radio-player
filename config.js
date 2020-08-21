af7fa51c7afcc58e19c9c6fb09fc67eee81e0288
mk2box <41782663+mk2box@users.noreply.github.com>
2020-08-21 14:44:32 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+/*
+|--------------------------------------------------------------------------
+| Конфигурација на сктирптата
+|--------------------------------------------------------------------------
+|
+| За да го конфигурирате стримот треба да ги промените овие сетинзи
+|
+*/
+
+var settings = {
+    'radio_name': 'Kanal77', // името на радио станицата
+    'url_streaming': 'https://radio.kanal77.mk:9002', // од каде го зима 7.html
+	'url_streaming2': 'https://live.kanal77.com.mk:8089/acc', // од каде го пушта аудио стримот
+    'streaming_type': 'shoutcast', // вид на стимот
+    'api_key': 'cae6a7d0071ec26ee581082dc65eefdf', // клуч за апито
+    'historic': true,
+    'next_song': false, // се користи само е shoutcast за да ја прикаже следната песна што иде
+    'default_cover_art': 'img/bg-capa.jpg', // позадинска слика на песните што неможат да се препознаат
+};
+
+const RADIO_NAME = settings.radio_name;
+const URL_STREAMING = settings.url_streaming;
+const URL_STREAMING2 = settings.url_streaming2;
+const STREAMING_TYPE = settings.streaming_type;
+const API_KEY = settings.api_key;
+const HISTORIC = settings.historic;
+const NEXT_SONG = settings.next_song;
+const DEFAULT_COVER_ART = settings.default_cover_art;
